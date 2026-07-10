3500d095df2e341ef3373cfc6b973c0190f620f4
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-07-10 16:01:09 +0530

    Time: 2 ms (70.26%), Space: 56 MB (6.57%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums){
+
+        int count = 0;
+        int freq = 0;
+        int ans = 0;
+
+        for(int i = 0;i<nums.length;i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(nums[i]==ans){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+}
