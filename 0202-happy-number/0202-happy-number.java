dcba55efa14a9d54d022b5bb29656401dcaec44a
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-07-13 15:48:34 +0530

    Time: 1 ms (78.21%), Space: 42.5 MB (51.22%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = findsquare(slow);
+            fast = findsquare(findsquare(fast));
+        }while(fast!=slow);
+
+
+
+        if(slow == 1){
+            return true;
+    }return false;
+    }
+
+    public static int findsquare(int n){
+        int ans = 0;
+        while(n>0){
+            int rem = n%10;
+            ans =ans + rem*rem;
+            n = n/10;
+        }
+        return ans;
+    }
+}
