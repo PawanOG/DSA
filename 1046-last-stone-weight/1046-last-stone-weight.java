ce91fac0edd84dd8243d66b50df7e621795546a6
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-07-25 21:15:13 +0530

    Time: 1 ms (99.59%), Space: 42.8 MB (70.12%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,19 +1,24 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 class Solution {
     public int lastStoneWeight(int[] stones) {
-     
-     PriorityQueue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
-     int k = stones.length;
 
-     for(int stone:stones){
-       maxheap.offer(stone);
-     }
-     while(maxheap.size()>1){
-        int a = maxheap.poll();
-        int b = maxheap.poll();
-        
-        maxheap.offer(Math.abs(a-b));
-    }
-    return maxheap.peek();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+            int a = maxHeap.poll();
+            int b = maxHeap.poll();
+
+            if (a != b) {
+                maxHeap.offer(a - b);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
     }
 }
