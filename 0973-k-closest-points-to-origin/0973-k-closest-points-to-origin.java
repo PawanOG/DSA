51964d7a04d4278a5571608f8a04084867f7031d
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-07-26 13:20:54 +0530

    Time: 30 ms (66.5%), Space: 54 MB (56.79%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> maxheap = new PriorityQueue<>(
+                (a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
+
+        for (int[] point : points) {
+            maxheap.offer(point);
+
+            if (maxheap.size() > k) {
+                maxheap.poll();
+            }
+        }
+
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            ans[i] = maxheap.poll();
+        }
+        return ans;
+
+    }
+}
