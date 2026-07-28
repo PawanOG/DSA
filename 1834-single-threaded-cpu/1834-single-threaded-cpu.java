1b7f1ed062c4f43b50d5ffa3deb97888fa4ce62a
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-07-29 00:49:05 +0530

    Time: 158 ms (29.43%), Space: 266.5 MB (15.11%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,43 +1,53 @@
+import java.util.*;
+
 class Solution {
     public int[] getOrder(int[][] tasks) {
+
         int n = tasks.length;
+
         int[][] arr = new int[n][3];
 
-        for(int i = 0;i<n;i++){
-            arr[i][0]=tasks[i][0]; // enque time
-            arr[i][1]=tasks[i][1]; //process time
-            arr[i][2]=i; //index of that task // mainly to return the ans in index terms
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = tasks[i][0]; // enqueue time
+            arr[i][1] = tasks[i][1]; // processing time
+            arr[i][2] = i;           // original index
         }
 
-        Arrays.sort(arr,(a,b) -> Integer.compare(a[0],b[0]));
-        
-        PriorityQueue<int[]> minheap = new PriorityQueue<>((a,b)->a[1]==b[1]?a[2]-b[2]:a[1]-b[1]);
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+            (a, b) -> {
+                if (a[1] == b[1])
+                    return Integer.compare(a[2], b[2]);
+                return Integer.compare(a[1], b[1]);
+            }
+        );
 
         int[] ans = new int[n];
-
-        int i = 0;
-        int idx=0;
-
+        int i = 0, idx = 0;
         long time = 0;
 
-        while(idx<n){
+        while (idx < n) {
 
-            if(minheap.isEmpty() && time<arr[i][0]){
+            // If no available task, move time forward
+            if (minHeap.isEmpty() && time < arr[i][0]) {
                 time = arr[i][0];
             }
 
-            while(i<n && arr[i][0]<=time){
-                minheap.offer(arr[i]);
+            // Add all available tasks
+            while (i < n && arr[i][0] <= time) {
+                minHeap.offer(arr[i]);
                 i++;
             }
 
-            if(!minheap.isEmpty()){
-                int[] task = minheap.poll();
-                ans[idx++]=task[2];
-                time = time + task[1];
-
+            // Execute shortest task
+            if (!minHeap.isEmpty()) {
+                int[] task = minHeap.poll();
+                ans[idx++] = task[2];
+                time += task[1];
             }
         }
+
         return ans;
     }
 }
