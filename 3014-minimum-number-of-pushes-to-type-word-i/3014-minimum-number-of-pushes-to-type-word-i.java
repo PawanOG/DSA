86d0eff3b2aec3836f36f87a465560c90d99f8b5
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-07-31 01:27:23 +0530

    Time: 3 ms (11.35%), Space: 43.4 MB (16.67%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Arrays.sort(freq);
+
+        int ans = 0;
+
+        for (int i = 25; i >= 0; i--) {
+            if (freq[i] == 0) break;
+
+            int pushes = ((25 - i) / 8) + 1;
+            ans += freq[i] * pushes;
+        }
+
+        return ans;
+    }
+}
