575667d46c43832896d5c1c4feca70844d305bd0
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-01 21:44:09 +0530

    Time: 66 ms (23.33%), Space: 43 MB (10.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,39 +1,16 @@
-
-
 class Solution {
 
     public boolean predictTheWinner(int[] nums) {
-        int sum = 0;
-
-        for (int x : nums)
-            sum += x;
-
-        int p1 = solve(0, nums.length - 1, nums);
-        int p2 = sum - p1;
-
-        return p1 >= p2;
+        return solve(0, nums.length - 1, nums) >= 0;
     }
 
     private int solve(int i, int j, int[] nums) {
 
-        if (i > j)
-            return 0;
-
         if (i == j)
             return nums[i];
 
-        //refer to  notes for this part 
-        //mainly we are choosing the worst possible conditions opponent left for us after his turn we taking minimun 
-
-        int takeLeft = nums[i] +
-                Math.min(
-                        solve(i + 2, j, nums),
-                        solve(i + 1, j - 1, nums));
-
-        int takeRight = nums[j] +
-                Math.min(
-                        solve(i + 1, j - 1, nums),
-                        solve(i, j - 2, nums));
+        int takeLeft = nums[i] - solve(i + 1, j, nums);
+        int takeRight = nums[j] - solve(i, j - 1, nums);
 
         return Math.max(takeLeft, takeRight);
     }
