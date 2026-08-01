3d3f8dc4953382e063f47b71d0b2e74ccaf10136
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-01 21:24:06 +0530

    Time: 74 ms (14%), Space: 42.6 MB (74.96%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,36 +1,37 @@
 
 
 class Solution {
-    public boolean predictTheWinner(int[] nums){
 
-        int n = nums.length;
+    public boolean predictTheWinner(int[] nums) {
         int sum = 0;
 
-        for(int num:nums){
-            sum += num;
-        }
-        int p1=solve(0,n-1,nums);
-        int p2 = sum-p1;
+        for (int x : nums)
+            sum += x;
 
-        if(p1>=p2){
-            return true;
-        }return false;
+        int p1 = solve(0, nums.length - 1, nums);
+        int p2 = sum - p1;
 
-        
+        return p1 >= p2;
     }
 
-    public int solve(int i,int j,int[] nums){
-          if(i>j){
+    private int solve(int i, int j, int[] nums) {
+
+        if (i > j)
             return 0;
-          }
 
-          if(i==j){
+        if (i == j)
             return nums[i];
-          }
 
-          int take_i = nums[i]+Math.min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-          int take_j = nums[j]+Math.min(solve(i+1,j-1,nums),solve(i,j-2,nums));
+        int takeLeft = nums[i] +
+                Math.min(
+                        solve(i + 2, j, nums),
+                        solve(i + 1, j - 1, nums));
 
-          return Math.max(take_i,take_j);
+        int takeRight = nums[j] +
+                Math.min(
+                        solve(i + 1, j - 1, nums),
+                        solve(i, j - 2, nums));
+
+        return Math.max(takeLeft, takeRight);
     }
 }
