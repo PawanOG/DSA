b05471072fefe6c1282e16b1b74049563f249eaf
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-01 21:22:12 +0530

    Time: 76 ms (12.75%), Space: 43 MB (23.56%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums){
+
+        int n = nums.length;
+        int sum = 0;
+
+        for(int num:nums){
+            sum += num;
+        }
+        int p1=solve(0,n-1,nums);
+        int p2 = sum-p1;
+
+        if(p1>=p2){
+            return true;
+        }return false;
+
+        
+    }
+
+    public int solve(int i,int j,int[] nums){
+          if(i>j){
+            return 0;
+          }
+
+          if(i==j){
+            return nums[i];
+          }
+
+          int take_i = nums[i]+Math.min(solve(i+2,j,nums),solve(i+1,j-1,nums));
+          int take_j = nums[j]+Math.min(solve(i+1,j-1,nums),solve(i,j-2,nums));
+
+          return Math.max(take_i,take_j);
+    }
+}
