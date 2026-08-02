dd573aeffd5361bc34e0fd4fd7db33f70bc8b08a
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-02 12:23:32 +0530

    Time: 2 ms (61.75%), Space: 46.2 MB (79.07%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+
+        int[] changesinlocation = new int[1001];
+
+        for(int[] trip : trips){
+           int passengers = trip[0];
+           int from = trip[1];
+           int to = trip[2];
+           
+           changesinlocation[from] = changesinlocation[from] + passengers;
+           changesinlocation[to] = changesinlocation[to] - passengers; 
+        }
+
+        int usedcapacity=0;
+
+        for(int changeincapacity : changesinlocation ){
+            usedcapacity = usedcapacity + changeincapacity;
+            if(usedcapacity>capacity){
+                return false;
+            }
+        }
+        return true;
+
+        
+    }
+}
