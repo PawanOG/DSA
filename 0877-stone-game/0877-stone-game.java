98c7dae912e0bf23de8a2f0cee9eeb219765d969
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-02 09:17:35 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.69%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,41 +1,5 @@
 class Solution {
-     Integer[][] dp;
     public boolean stoneGame(int[] piles) {
-
-        int n = piles.length;
-        dp = new Integer[n][n];
-        int total = 0;
-
-        for(int x : piles){
-            total += x;
+        return true;
         }
-
-        int alicescore = solve(0,n-1,piles);
-        int bobscore = total - alicescore;
-
-        if(alicescore>bobscore){
-            return true;
-        }return false;
-
-        
-    }
-
-    public int solve(int i ,int j ,int[] piles){
-
-        if(i==j){
-            return piles[i];
-        }
-
-        if(i>j){
-            return 0;
-        }
-        
-        if (dp[i][j] != null)
-            return dp[i][j];
-    
-        int take_i = piles[i]+Math.min(solve(i+2,j,piles),solve(i+1,j-1,piles));
-        int take_j = piles[j]+Math.min(solve(i+1,j-1,piles),solve(i,j-2,piles));
-
-        return dp[i][j]=Math.max(take_i,take_j);
-    }
 }
