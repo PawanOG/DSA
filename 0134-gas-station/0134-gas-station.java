b21db51a1344283f8de61b15361c12596c47ef63
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-04 14:44:50 +0530

    Time: 2 ms (99.25%), Space: 119.4 MB (65.99%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,32 +1,19 @@
-
-
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-
-        int[] gain = new int[cost.length];
-        int totalcost = 0;
-        int totalgas = 0;
+        int gain = 0;
+        int totalgain = 0;
         int currentgain = 0;
         int start = 0;
-        //ham har station ka gain ,gain array ke usi index pe store krde denge jis index pe station h
-        for (int i = 0; i < cost.length; i++) {
-            gain[i] = gas[i] - cost[i];
-            totalcost += cost[i];
-            totalgas += gas[i];
-        }
-
-        if (totalcost > totalgas) {
-            return -1;
-        } else {
-            //now we are trying to find the station which will be best for our stating point
-            for (int i = 0; i < gain.length; i++) {
-                currentgain += gain[i];
-                if (currentgain < 0) {
-                    currentgain = 0;
-                    start = i + 1;
-                }
+        for(int i = 0;i<gas.length;i++){
+            gain = gas[i]-cost[i];
+            totalgain+=gain;
+            currentgain+=gain;
+            if(currentgain<0){
+                currentgain = 0;
+                start = i+1;
             }
         }
-        return start;
+        return totalgain<0?-1:start;
+       
     }
 }
