42336230ea40cfdecb60da66b423e72ccd56529d
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-05 16:29:09 +0530

    Time: 0 ms (100%), Space: 44.8 MB (59.12%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,38 +1,16 @@
-
-
 class Solution {
-    int[] dp;
-
     public int minCostClimbingStairs(int[] cost) {
-
         int n = cost.length;
-        dp = new int[n];
+        int[] dp = new int[n];
 
-        //in many questions 0 sometimes can be part of answer so we cant keep 0 as not computed in many dp arrays so here weare keeping -1
-         Arrays.fill(dp, -1);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
 
+        //solving using dp + tabulation/iterative method
 
-        return Math.min(
-                mincost(cost, n - 1) + cost[n - 1],
-                mincost(cost, n - 2) + cost[n - 2]
-        );
-
-    }
-
-    public int mincost(int[] cost, int i) {
-
-        if (i <= 1) {
-            return 0;
+        for(int i = 2;i<n;i++){
+            dp[i]=cost[i] + Math.min(dp[i-1],dp[i-2]);
         }
-        
-        //0's are part of answer in some test cases of dp so we cant assume 0 as not computed so keeping -1 inplace of 0
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        dp[i] = Math.min(cost[i - 1] + mincost(cost, i - 1),
-                         cost[i - 2] + mincost(cost, i - 2));
-
-        return dp[i];
+        return Math.min(dp[n-1],dp[n-2]);
     }
 }
