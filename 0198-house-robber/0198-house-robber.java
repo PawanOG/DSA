1f312fcd2b5c67f340044f7c29e67042937ea420
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-07 16:33:01 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -7,15 +7,18 @@ class Solution {
           if (n == 1) {
             return nums[0];
         }
-        int[] dp = new int[n];
+        int[] dp = new int[3];
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
 
         for(int i = 2;i<nums.length;i++){
-            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+            dp[2]=Math.max(dp[0]+nums[i],dp[1]);
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+
         }
 
-        return dp[n-1];
+        return dp[1];
 
     }
 }
