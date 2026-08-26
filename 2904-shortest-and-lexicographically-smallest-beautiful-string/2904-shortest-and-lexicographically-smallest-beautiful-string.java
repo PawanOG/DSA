d3dd8743e108ab7ec932af5afbdeb8c117dbbc21
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-08-27 00:56:32 +0530

    Time: 21 ms (5.37%), Space: 47.2 MB (6.04%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -2,33 +2,36 @@
 
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-           
-           int n = s.length();
-           String ans = "";
-           
-           for(int i = 0;i<n;i++){
-                
-                int onecnt = 0;
-                String curr = "";
-                
-                for(int j = i;j<n;j++){
-                    curr = curr + s.charAt(j);
 
-                    if(s.charAt(j)=='1'){
-                        onecnt++;
-                    }
+        int n = s.length();
+        String ans = "";
 
-                    if(onecnt>k){
-                        break;
-                    }
+        for (int i = 0; i < n; i++) {
 
-                    if(onecnt == k){
-                        if(ans.isEmpty() || curr.length() < ans.length() || (curr.length() == ans.length() && curr.compareTo(ans) < 0)){
-                            ans = curr;
-                        }
+            int onecnt = 0;
+            String curr = "";
+
+            for (int j = i; j < n; j++) {
+
+                curr = curr + s.charAt(j);
+
+                if (s.charAt(j) == '1') {
+                    onecnt++;
+                }
+
+                if (onecnt > k) {
+                    break;
+                }
+
+                if (onecnt == k) {
+                    if (ans.isEmpty() || curr.length() < ans.length()
+                            || (curr.length() == ans.length() && curr.compareTo(ans) < 0)) {
+                        ans = curr;
                     }
                 }
-           }
-           return ans;
+            }
+        }
+
+        return ans;
     }
 }
