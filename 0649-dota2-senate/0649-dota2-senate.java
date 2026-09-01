1bc9d6da2cea587445829f3f4b8ec7b3b3bcb550
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-09-01 23:43:50 +0530

    Time: 222 ms (5.25%), Space: 43.7 MB (98.97%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    int n;
+
+    void removeSenator(String senate, boolean[] removed, char ch, int idx) {
+
+        while (true) {
+
+            if (senate.charAt(idx) == ch && removed[idx] == false) {
+                removed[idx] = true;
+                break;
+            }
+
+            idx = (idx + 1) % n;
+        }
+    }
+
+    public String predictPartyVictory(String senate) {
+
+        n = senate.length();
+
+        int R_Count = 0;
+        int D_Count = 0;
+
+        
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                R_Count++;
+            }
+        }
+
+        D_Count = n - R_Count;
+
+        int idx = 0;
+
+        
+        boolean[] removed = new boolean[n];
+
+        while (R_Count > 0 && D_Count > 0) {
+
+            if (!removed[idx]) {
+
+                if (senate.charAt(idx) == 'R') {
+
+                    removeSenator(
+                        senate,
+                        removed,
+                        'D',
+                        (idx + 1) % n
+                    );
+
+                    D_Count--;
+
+                } else {
+
+                    removeSenator(
+                        senate,
+                        removed,
+                        'R',
+                        (idx + 1) % n
+                    );
+
+                    R_Count--;
+                }
+            }
+
+            idx = (idx + 1) % n;
+        }
+
+        return R_Count == 0 ? "Dire" : "Radiant";
+    }
+}
