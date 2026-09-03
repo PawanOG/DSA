201320f660cdaa14dd8399c5ae8462903abe0195
Pawan Gupta <142137375+PawanOG@users.noreply.github.com>
2026-09-03 21:10:08 +0530

    Time: 5 ms (83.33%), Space: 121.6 MB (74.64%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean uniformArray(int[] nums1) {
         int evens = 0;
         int odds = 0;
-        Arrays.sort(nums1);
         int min = Integer.MAX_VALUE;
         for(int val : nums1){
             min = Math.min(min,val);
